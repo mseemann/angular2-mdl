63a3e8f37b0a336ffd7ed1b65735427b92ef5b40
Michael Seemann <seemann@mseemann.de>
2016-06-08 14:22:10 +0200

    changes output folder for scss files

--- dev-scripts/generate-scss-from-mdl-scss.js
+++ dev-scripts/generate-scss-from-mdl-scss.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 'use strict';
 
+// extract scss files and put them into scss-mdl folder. Run: npm run generate-scss-from-mdl-scss
+
 const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
@@ -8,10 +10,10 @@ const ncp = require('ncp').ncp;
 
 var basePath = process.cwd();
 var source = path.resolve(basePath, 'node_modules/material-design-lite/src');
-var dest = path.resolve(basePath, 'src/scss/mdl');
+var dest = path.resolve(basePath, 'src/scss-mdl');
 
 
-fs.mkdirSync(path.join('src/scss','mdl'));
+fs.mkdirSync('src/scss-mdl');
 
 ncp(source, dest, {filter: function(fileName){
   if(fileName.endsWith('snippets')){
@@ -20,7 +22,7 @@ ncp(source, dest, {filter: function(fileName){
   if(fs.statSync(fileName).isDirectory()){
     return true;
   }
-  return fileName.endsWith('.scss') || fileName.endsWith('.svg');
+  return fileName.endsWith('.scss') || fileName.endsWith('.scss');
 }}, function (err) {
   if (err) {
     return console.error(err);
