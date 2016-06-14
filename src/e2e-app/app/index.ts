57153134de93b94c8e6af571d35c53f071ea1865
Michael Seemann <seemann@mseemann.de>
2016-06-14 14:57:46 +0200

    fix prod build problems

--- src/e2e-app/app/index.ts
+++ src/e2e-app/app/index.ts
@@ -1,2 +1,3 @@
 export * from './environment';
 export * from './angular2-mdl.component';
+export * from './app.routes';
--- src/e2e-app/main.ts
+++ src/e2e-app/main.ts
@@ -1,7 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { Angular2MdlAppComponent, environment } from './app/';
-import { APP_ROUTER_PROVIDERS } from './app/app.routes';
+import { APP_ROUTER_PROVIDERS } from './app/';
 
 if (environment.production) {
   enableProdMode();
