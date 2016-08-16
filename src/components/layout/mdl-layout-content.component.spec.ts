f13da06b2ef27124a4aa621d116f43622b292405
Michael Seemann <seemann@mseemann.de>
2016-08-16 12:20:45 +0200

    MdlLayout: switch to TestBed;

--- src/components/layout/mdl-layout-content.component.spec.ts
+++ src/components/layout/mdl-layout-content.component.spec.ts
@@ -1,34 +1,30 @@
-import {
-  inject,
-  TestComponentBuilder
-} from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { Component } from '@angular/core';
 import { MdlLayoutContentComponent } from './mdl-layout-content.component';
+import { MdlLayoutModule } from './index';
 
 describe('Component: MdlLayoutContent', () => {
 
-  var builder: TestComponentBuilder;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ MdlLayoutModule ],
+      declarations: [ MdlTestLayoutComponent ],
+    });
 
-  beforeEach(inject([TestComponentBuilder], function (tcb: TestComponentBuilder) {
-    builder = tcb;
-  }));
+  });
 
   it('should add the css class mdl-layout__content to the host element', ( done ) => {
 
-    return builder
-      .overrideTemplate(MdlTestLayoutComponent, `
-          <mdl-layout-content>x</mdl-layout-content>
-        `)
-      .createAsync(MdlTestLayoutComponent).then( (fixture) => {
+    let fixture = TestBed.createComponent(MdlTestLayoutComponent);
 
-        fixture.detectChanges();
+    fixture.detectChanges();
 
-        let layoutEl: HTMLElement = fixture.debugElement.query(By.directive(MdlLayoutContentComponent)).nativeElement;
-        expect(layoutEl.classList.contains('mdl-layout__content')).toBe(true);
+    let layoutEl: HTMLElement = fixture.debugElement.query(By.directive(MdlLayoutContentComponent)).nativeElement;
+    expect(layoutEl.classList.contains('mdl-layout__content')).toBe(true);
 
-        done();
-      });
+    done();
+
   });
 
 
@@ -37,7 +33,7 @@ describe('Component: MdlLayoutContent', () => {
 
 @Component({
   selector: 'test-layout',
-  template: 'replaced by the test',
+  template: '<mdl-layout-content>x</mdl-layout-content>',
   directives: [MdlLayoutContentComponent]
 })
 class MdlTestLayoutComponent {}
