3e30522e001ab85386a9e28da2ef24a70eb5da3f
Michael Seemann <seemann@mseemann.de>
2016-08-16 09:28:28 +0200

    MdlCard: switch to TestBed

--- src/components/card/mdl-card.component.spec.ts
+++ src/components/card/mdl-card.component.spec.ts
@@ -1,138 +1,121 @@
-import {
-  inject,
-  TestComponentBuilder,
-  TestBed,
-  async
-} from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { Component } from '@angular/core';
 import { MdlCardModule } from './mdl-card.component';
 
 describe('Components: MdlCard*', () => {
 
-  var builder: TestComponentBuilder;
-
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [MdlCardModule],
-      declarations: [],
+      declarations: [TestApp],
     });
+  });
 
-    TestBed.compileComponents();
-  }));
-
-  beforeEach(inject([TestComponentBuilder], function (tcb: TestComponentBuilder) {
-    builder = tcb;
-  }));
 
   it('should add the css class mdl-card to the element', ( done ) => {
 
-    return builder
-      .createAsync(TestApp).then( (fixture) => {
+    let fixture = TestBed.createComponent(TestApp);
+    fixture.detectChanges();
 
-        fixture.detectChanges();
+    let mdlCardElement = fixture.debugElement.query(By.css('mdl-card'));
+    expect(mdlCardElement.nativeElement.classList.contains('mdl-card')).toBe(true);
 
-        let mdlCardElement = fixture.debugElement.query(By.css('mdl-card'));
-        expect(mdlCardElement.nativeElement.classList.contains('mdl-card')).toBe(true);
+    let mdlCardTitleElement = fixture.debugElement.query(By.css('mdl-card-title'));
+    expect(mdlCardTitleElement.nativeElement.classList.contains('mdl-card__title')).toBe(true);
 
-        let mdlCardTitleElement = fixture.debugElement.query(By.css('mdl-card-title'));
-        expect(mdlCardTitleElement.nativeElement.classList.contains('mdl-card__title')).toBe(true);
+    let mdlCardMediaElement = fixture.debugElement.query(By.css('mdl-card-media'));
+    expect(mdlCardMediaElement.nativeElement.classList.contains('mdl-card__media')).toBe(true);
 
-        let mdlCardMediaElement = fixture.debugElement.query(By.css('mdl-card-media'));
-        expect(mdlCardMediaElement.nativeElement.classList.contains('mdl-card__media')).toBe(true);
+    let mdlCardSupportingTextElement = fixture.debugElement.query(By.css('mdl-card-supporting-text'));
+    expect(mdlCardSupportingTextElement.nativeElement.classList.contains('mdl-card__supporting-text')).toBe(true);
 
-        let mdlCardSupportingTextElement = fixture.debugElement.query(By.css('mdl-card-supporting-text'));
-        expect(mdlCardSupportingTextElement.nativeElement.classList.contains('mdl-card__supporting-text')).toBe(true);
+    let mdlCardActionsElement = fixture.debugElement.query(By.css('mdl-card-actions'));
+    expect(mdlCardActionsElement.nativeElement.classList.contains('mdl-card__actions')).toBe(true);
 
-        let mdlCardActionsElement = fixture.debugElement.query(By.css('mdl-card-actions'));
-        expect(mdlCardActionsElement.nativeElement.classList.contains('mdl-card__actions')).toBe(true);
+    let mdlCardMenuElement = fixture.debugElement.query(By.css('mdl-card-menu'));
+    expect(mdlCardMenuElement.nativeElement.classList.contains('mdl-card__menu')).toBe(true);
 
-        let mdlCardMenuElement = fixture.debugElement.query(By.css('mdl-card-menu'));
-        expect(mdlCardMenuElement.nativeElement.classList.contains('mdl-card__menu')).toBe(true);
+    let mdlCardTitleTextElement = fixture.debugElement.query(By.css('[mdl-card-title-text]'));
+    expect(mdlCardTitleTextElement.nativeElement.classList.contains('mdl-card__title-text')).toBe(true);
 
-        let mdlCardTitleTextElement = fixture.debugElement.query(By.css('[mdl-card-title-text]'));
-        expect(mdlCardTitleTextElement.nativeElement.classList.contains('mdl-card__title-text')).toBe(true);
+    let mdlCardBorderElement = fixture.debugElement.query(By.css('[mdl-card-border]'));
+    expect(mdlCardBorderElement.nativeElement.classList.contains('mdl-card--border')).toBe(true);
 
-        let mdlCardBorderElement = fixture.debugElement.query(By.css('[mdl-card-border]'));
-        expect(mdlCardBorderElement.nativeElement.classList.contains('mdl-card--border')).toBe(true);
+    let mdlCardExpandElement = fixture.debugElement.query(By.css('[mdl-card-expand]'));
+    expect(mdlCardExpandElement.nativeElement.classList.contains('mdl-card--expand')).toBe(true);
 
-        let mdlCardExpandElement = fixture.debugElement.query(By.css('[mdl-card-expand]'));
-        expect(mdlCardExpandElement.nativeElement.classList.contains('mdl-card--expand')).toBe(true);
+    done();
 
-        done();
-      });
   });
 
   it('should throw if mdl-card-title has no mdl-card parent', ( done ) => {
-    return builder
-      .overrideTemplate(TestApp, `
-          <mdl-card-title></mdl-card-title>
-        `)
-      .createAsync(TestApp).then( (fixture) => {
 
-        expect( () => fixture.detectChanges() )
-          .toThrow();
+    TestBed.overrideComponent(TestApp, { set: {
+      template: '<mdl-card-title></mdl-card-title>' }
+    });
+    let fixture = TestBed.createComponent(TestApp);
 
-        done();
-      });
+    expect( () => fixture.detectChanges() )
+      .toThrow();
+
+    done();
+
   });
 
   it('should throw if mdl-card-supporting-text has no mdl-card parent', ( done ) => {
-    return builder
-      .overrideTemplate(TestApp, `
-          <mdl-card-supporting-text></mdl-card-supporting-text>
-        `)
-      .createAsync(TestApp).then( (fixture) => {
 
-        expect( () => fixture.detectChanges() )
-          .toThrow();
+    TestBed.overrideComponent(TestApp, { set: {
+      template: '<mdl-card-supporting-text></mdl-card-supporting-text>' }
+    });
+    let fixture = TestBed.createComponent(TestApp);
 
-        done();
-      });
+    expect( () => fixture.detectChanges() )
+      .toThrow();
+
+    done();
+
   });
 
   it('should throw if mdl-card-actions has no mdl-card parent', ( done ) => {
-    return builder
-      .overrideTemplate(TestApp, `
-          <mdl-card-actions></mdl-card-actions>
-        `)
-      .createAsync(TestApp).then( (fixture) => {
 
-        expect( () => fixture.detectChanges() )
-          .toThrow();
+    TestBed.overrideComponent(TestApp, { set: {
+      template: '<mdl-card-actions></mdl-card-actions>' }
+    });
+    let fixture = TestBed.createComponent(TestApp);
 
-        done();
+    expect( () => fixture.detectChanges() )
+      .toThrow();
 
-      });
+    done();
+
   });
 
   it('should throw if mdl-card-menu has no mdl-card parent', ( done ) => {
-    return builder
-      .overrideTemplate(TestApp, `
-          <mdl-card-menu></mdl-card-menu>
-        `)
-      .createAsync(TestApp).then( (fixture) => {
 
-        expect( () => fixture.detectChanges() )
-          .toThrow();
+    TestBed.overrideComponent(TestApp, { set: {
+      template: '<mdl-card-menu></mdl-card-menu>' }
+    });
+    let fixture = TestBed.createComponent(TestApp);
 
-        done();
+    expect( () => fixture.detectChanges() )
+      .toThrow();
 
-      });
+    done();
+
   });
 
   it('should throw if mdl-card-media has no mdl-card parent', ( done ) => {
-    return builder
-      .overrideTemplate(TestApp, `
-          <mdl-card-media></mdl-card-media>
-        `)
-      .createAsync(TestApp).then( (fixture) => {
 
-        expect( () => fixture.detectChanges() )
-          .toThrow();
+    TestBed.overrideComponent(TestApp, { set: {
+      template: '<mdl-card-media></mdl-card-media>' }
+    });
+    let fixture = TestBed.createComponent(TestApp);
+    
+    expect( () => fixture.detectChanges() )
+      .toThrow();
 
-        done();
-      });
+    done();
   });
 
 });
