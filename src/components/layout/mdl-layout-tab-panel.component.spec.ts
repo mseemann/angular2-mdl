9c72196a55aa401b8b1552ee272580e7a06704a4
Michael Seemann <seemann@mseemann.de>
2016-08-16 14:18:18 +0200

    MdlLayout: switch to TestBed;

--- src/components/layout/mdl-layout-tab-panel.component.spec.ts
+++ src/components/layout/mdl-layout-tab-panel.component.spec.ts
@@ -1,58 +1,50 @@
-import {
-  inject,
-  TestComponentBuilder
-} from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { Component } from '@angular/core';
-import { MdlLayoutTabPanelComponent } from './mdl-layout-tab-panel.component';
+import { MdlLayoutModule, MdlLayoutTabPanelComponent } from './index';
 
 describe('Component: MdlLayoutTabPanel', () => {
 
-  var builder: TestComponentBuilder;
-
-  beforeEach(inject([TestComponentBuilder], function (tcb: TestComponentBuilder) {
-    builder = tcb;
-  }));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ MdlLayoutModule ],
+      declarations: [ MdlTestComponent ]
+    });
+  });
 
   it('should add the css class mdl-layout__tab-panel to the host element', ( done ) => {
 
-    return builder
-      .overrideTemplate(MdlTestComponent, `
-          <mdl-layout-tab-panel>x</mdl-layout-tab-panel>
-        `)
-      .createAsync(MdlTestComponent).then( (fixture) => {
+    TestBed.overrideComponent(MdlTestComponent, { set: {
+      template: '<mdl-layout-tab-panel>x</mdl-layout-tab-panel>' }
+    });
+    let fixture = TestBed.createComponent(MdlTestComponent);
+    fixture.detectChanges();
 
-        fixture.detectChanges();
+    let tabPanelEl: HTMLElement = fixture.nativeElement.children.item(0);
+    expect(tabPanelEl.classList.contains('mdl-layout__tab-panel')).toBe(true);
 
-        let tabPanelEl: HTMLElement = fixture.nativeElement.children.item(0);
-        expect(tabPanelEl.classList.contains('mdl-layout__tab-panel')).toBe(true);
-
-        done();
-      });
+    done();
 
   });
 
   it('should add the css class isActive to the host element if the panel is active', ( done ) => {
 
-    return builder
-      .overrideTemplate(MdlTestComponent, `
-          <mdl-layout-tab-panel>x</mdl-layout-tab-panel>
-        `)
-      .createAsync(MdlTestComponent).then( (fixture) => {
+    TestBed.overrideComponent(MdlTestComponent, { set: {
+      template: '<mdl-layout-tab-panel>x</mdl-layout-tab-panel>' }
+    });
+    let fixture = TestBed.createComponent(MdlTestComponent);
+    fixture.detectChanges();
 
-        fixture.detectChanges();
+    let tabPanelComponent = fixture.debugElement.query(By.directive(MdlLayoutTabPanelComponent)).componentInstance;
 
-        let tabPanelComponent = fixture.debugElement.query(By.directive(MdlLayoutTabPanelComponent)).componentInstance;
+    tabPanelComponent.isActive = true;
+    fixture.detectChanges();
 
-        tabPanelComponent.isActive = true;
-        fixture.detectChanges();
+    let tabPanelEl: HTMLElement = fixture.nativeElement.children.item(0);
 
-        let tabPanelEl: HTMLElement = fixture.nativeElement.children.item(0);
+    expect(tabPanelEl.classList.contains('is-active')).toBe(true);
 
-        expect(tabPanelEl.classList.contains('is-active')).toBe(true);
-
-        done();
-      });
+    done();
 
   });
 
@@ -61,7 +53,6 @@ describe('Component: MdlLayoutTabPanel', () => {
 
 @Component({
   selector: 'test',
-  template: 'replaced by the test',
-  directives: [MdlLayoutTabPanelComponent]
+  template: 'replaced by the test'
 })
 class MdlTestComponent {}
