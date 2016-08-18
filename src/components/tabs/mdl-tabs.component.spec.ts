e8081b7a47995d8dfd40db35ac5d0fa49576b6a8
Michael Seemann <seemann@mseemann.de>
2016-08-18 14:52:05 +0200

    Tabs -> TestBed

--- src/components/tabs/mdl-tabs.component.spec.ts
+++ src/components/tabs/mdl-tabs.component.spec.ts
@@ -1,104 +1,89 @@
-import {
-  inject,
-  TestComponentBuilder
-} from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { Component } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import {
-  MdlTabsComponent,
-  MdlTabPanelComponent,
-  MdlTabPanelTitleComponent
+  MdlTabsModule,
+  MdlTabsComponent
 } from './index';
 
 describe('Component: MdlTabs', () => {
 
-  var builder: TestComponentBuilder;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [MdlTabsModule],
+      declarations: [MdlTestComponent],
+    });
+  });
 
-  beforeEach(inject([TestComponentBuilder], function (tcb: TestComponentBuilder) {
-    builder = tcb;
-  }));
+  it('should add the css class mdl-tabs to the host element', () => {
 
-  it('should add the css class mdl-tabs to the host element', ( done ) => {
+    TestBed.overrideComponent(MdlTestComponent, { set: {
+      template: '<mdl-tabs>x</mdl-tabs>' }
+    });
+    let fixture = TestBed.createComponent(MdlTestComponent);
+    fixture.detectChanges();
 
-    return builder
-      .overrideTemplate(MdlTestComponent, `
-          <mdl-tabs>x</mdl-tabs>
-        `)
-      .createAsync(MdlTestComponent).then( (fixture) => {
-
-        fixture.detectChanges();
-
-        let tabsEl: HTMLElement = fixture.nativeElement.children.item(0);
-        expect(tabsEl.classList.contains('mdl-tabs')).toBe(true);
-
-        done();
-      });
+    let tabsEl: HTMLElement = fixture.nativeElement.children.item(0);
+    expect(tabsEl.classList.contains('mdl-tabs')).toBe(true);
 
   });
 
-  it('should activate the first tab if no index is set', ( done ) => {
+  it('should activate the first tab if no index is set', () => {
 
-    return builder
-      .overrideTemplate(MdlTestComponent, `
-          <mdl-tabs><mdl-tab-panel></mdl-tab-panel></mdl-tabs>
-        `)
-      .createAsync(MdlTestComponent).then( (fixture) => {
-
-        fixture.detectChanges();
+    TestBed.overrideComponent(MdlTestComponent, { set: {
+      template: '<mdl-tabs><mdl-tab-panel></mdl-tab-panel></mdl-tabs>' }
+    });
+    let fixture = TestBed.createComponent(MdlTestComponent);
+    fixture.detectChanges();
         
-        let mdlTabsComponent: MdlTabsComponent =
-          fixture.debugElement.query(By.directive(MdlTabsComponent)).componentInstance;
-        
-        expect(mdlTabsComponent.selectedIndex).toBe(0);
+    let mdlTabsComponent: MdlTabsComponent =
+      fixture.debugElement.query(By.directive(MdlTabsComponent)).componentInstance;
 
-        done();
-      });
+    expect(mdlTabsComponent.selectedIndex).toBe(0);
 
   });
 
-  it('should be possible to listen to chanegs to the active tab', ( done ) => {
+  it('should be possible to listen to chanegs to the active tab', () => {
 
-    return builder
-      .overrideTemplate(MdlTestComponent, `
+    TestBed.overrideComponent(MdlTestComponent, { set: {
+      template: `
           <mdl-tabs [mdl-tab-active-index]="1" (mdl-tab-active-changed)="tabChanged($event)">
             <mdl-tab-panel></mdl-tab-panel>
             <mdl-tab-panel></mdl-tab-panel>
           </mdl-tabs>
-        `)
-      .createAsync(MdlTestComponent).then( (fixture) => {
+        ` }
+    });
+    let fixture = TestBed.createComponent(MdlTestComponent);
+    fixture.detectChanges();
 
-        fixture.detectChanges();
+    let testComponent = fixture.componentInstance;
 
-        let testComponent = fixture.componentInstance;
+    let mdlTabsComponent: MdlTabsComponent =
+      fixture.debugElement.query(By.directive(MdlTabsComponent)).componentInstance;
 
-        let mdlTabsComponent: MdlTabsComponent =
-          fixture.debugElement.query(By.directive(MdlTabsComponent)).componentInstance;
+    expect(mdlTabsComponent.selectedIndex).toBe(1);
+    let aDebugElements = fixture.debugElement.queryAll(By.css('a'));
 
-        expect(mdlTabsComponent.selectedIndex).toBe(1);
-        let aDebugElements = fixture.debugElement.queryAll(By.css('a'));
+    // select the first tab
+    aDebugElements[0].nativeElement.click();
 
-        // select the first tab
-        aDebugElements[0].nativeElement.click();
+    fixture.detectChanges();
 
-        fixture.detectChanges();
-
-        expect(mdlTabsComponent.selectedIndex).toBe(0);
+    expect(mdlTabsComponent.selectedIndex).toBe(0);
 
 
-        expect(testComponent.selectedIndexOutput).toBe(0);
+    expect(testComponent.selectedIndexOutput).toBe(0);
 
-        // click again should change nothing
-        aDebugElements[0].nativeElement.click();
-        expect(mdlTabsComponent.selectedIndex).toBe(0);
+    // click again should change nothing
+    aDebugElements[0].nativeElement.click();
+    expect(mdlTabsComponent.selectedIndex).toBe(0);
 
-        done();
-      });
   });
 
-  it('should be possible to create rich tabs', ( done ) => {
+  it('should be possible to create rich tabs', () => {
 
-    return builder
-      .overrideTemplate(MdlTestComponent, `
+    TestBed.overrideComponent(MdlTestComponent, { set: {
+      template: `
           <mdl-tabs [mdl-tab-active-index]="1" (mdl-tab-active-changed)="tabChanged($event)">
             <mdl-tab-panel>
               <mdl-tab-panel-title>
@@ -109,26 +94,21 @@ describe('Component: MdlTabs', () => {
               </mdl-tab-panel-content>
             </mdl-tab-panel>
           </mdl-tabs>
-        `)
-      .createAsync(MdlTestComponent).then( (fixture) => {
+        ` }
+    });
+    let fixture = TestBed.createComponent(MdlTestComponent);
+    fixture.detectChanges();
 
-          fixture.detectChanges();
-
-          // the tab is now a div tag and not an a tag.
-          let testElement = fixture.debugElement.query(By.css('.mdl-tabs__tab'));
-          expect(testElement.nativeElement.nodeName).toBe('DIV');
-
-          done();
-      });
-
+    // the tab is now a div tag and not an a tag.
+    let testElement = fixture.debugElement.query(By.css('.mdl-tabs__tab'));
+    expect(testElement.nativeElement.nodeName).toBe('DIV');
   });
 });
 
 
 @Component({
   selector: 'test',
-  template: 'replaced by the test',
-  directives: [MdlTabsComponent, MdlTabPanelComponent, MdlTabPanelTitleComponent]
+  template: 'replaced by the test'
 })
 class MdlTestComponent {
 
