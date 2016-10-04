9b877e5c51332be2c4c5b8cc6be9ee39dffb23ab
tb <t.bak@selleo.com>
2016-10-04 23:23:18 +0200

    WIP - dialog defined in template
    Rename MdlDialogComponent to MdlDialogDefaultComponent
    Added MdlDialogComponent for adding dialog defined in template

--- src/lib/components/dialog/index.ts
+++ src/lib/components/dialog/index.ts
@@ -2,6 +2,7 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MdlDialogService } from './mdl-dialog.service';
 import { MdlDialogComponent } from './mdl-dialog.component';
+import { MdlDialogDefaultComponent } from './mdl-dialog.component';
 import { MdlCommonsModule } from '../common/index';
 import { MdlDialogHostComponent } from './mdl-dialog-host.component';
 
@@ -10,9 +11,9 @@ export * from './mdl-dialog-configuration';
 
 @NgModule({
   imports: [CommonModule, MdlCommonsModule],
-  exports: [MdlDialogComponent],
-  declarations: [MdlDialogComponent, MdlDialogHostComponent],
-  entryComponents: [MdlDialogComponent, MdlDialogHostComponent]
+  exports: [MdlDialogComponent, MdlDialogDefaultComponent],
+  declarations: [MdlDialogComponent, MdlDialogDefaultComponent, MdlDialogHostComponent],
+  entryComponents: [MdlDialogComponent, MdlDialogDefaultComponent, MdlDialogHostComponent]
 })
 export class MdlDialogModule {
   public static forRoot(): ModuleWithProviders {
