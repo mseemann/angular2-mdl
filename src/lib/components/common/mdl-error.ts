ef0230f882c0d3a93474c70fdeae2ecb30b51b88
Michael Seemann <seemann@mseemann.de>
2017-03-12 13:44:17 +0100

    test

--- src/lib/components/common/mdl-error.ts
+++ src/lib/components/common/mdl-error.ts
@@ -1,17 +1,16 @@
 /**
  * Wrapper for mdl error messages.
- * istanbul ignore next  LCOV_EXCL_LINE
  */
 export class MdlError extends Error {
   constructor(value: string) {
-    /* istanbul ignore next  LCOV_EXCL_LINE */
+    /* istanbul ignore next */
     super(value);
   }
 }
 
 export class MdlStructureError extends MdlError {
   constructor(child: string, requiredParent: string) {
-    /* istanbul ignore next LCOV_EXCL_LINE */
+    /* istanbul ignore next */
     super(`"${child}" requires "${requiredParent}" as a parent.`);
   }
 }
