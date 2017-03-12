8dd0bfb1676788c14a14ba00df1decfecb822672
Michael Seemann <seemann@mseemann.de>
2017-03-12 13:27:35 +0100

    improve test coverage

--- src/lib/components/common/mdl-error.ts
+++ src/lib/components/common/mdl-error.ts
@@ -3,14 +3,14 @@
  */
 export class MdlError extends Error {
   constructor(value: string) {
-    /* istanbul ignore next */
+    /* istanbul ignore next  LCOV_EXCL_LINE */
     super(value);
   }
 }
 
 export class MdlStructureError extends MdlError {
   constructor(child: string, requiredParent: string) {
-    /* istanbul ignore next */
+    /* istanbul ignore next LCOV_EXCL_LINE */
     super(`"${child}" requires "${requiredParent}" as a parent.`);
   }
 }
