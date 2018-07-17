61636d02c507e1e8a577bec48d75de47ab8abae1
Michael Seemann <seemann@mseemann.de>
2018-07-17 20:32:03 +0200

    fix externals for rxjs and angular

--- config/webpack/webpack.config.umd.js
+++ config/webpack/webpack.config.umd.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 var util = require('./util');
-const rxPaths = require('rxjs/_esm5/path-mapping');
+const angularExternals = require('webpack-angular-externals');
+const rxjsExternals = require('webpack-rxjs-externals');
 
 module.exports = {
 	entry: './src/lib/components/index.ts',
@@ -11,72 +12,12 @@ module.exports = {
 		library: 'angularMdlCore'
 	},
 	resolve: {
-		extensions: ['.ts', '.js'],
-		// Use the "alias" key to resolve to an ESM distribution
-		alias: rxPaths()
+		extensions: ['.ts', '.js']
 	},
-	externals: {
-		'@angular/core': {
-			root: ['ng', 'core'],
-			commonjs: '@angular/core',
-			commonjs2: '@angular/core',
-			amd: '@angular/core'
-		},
-		'@angular/common': {
-			root: ['ng', 'common'],
-			commonjs: '@angular/common',
-			commonjs2: '@angular/common',
-			amd: '@angular/common'
-		},
-		'@angular/forms': {
-			root: ['ng', 'forms'],
-			commonjs: '@angular/forms',
-			commonjs2: '@angular/forms',
-			amd: '@angular/forms'
-		},
-		'@angular/platform-browser': {
-			root: ['ng', 'platformBrowser'],
-			commonjs: '@angular/platform-browser',
-			commonjs2: '@angular/platform-browser',
-			amd: '@angular/platform-browser'
-		},
-		'@angular/platform-browser-dynamic': {
-			root: ['ng', 'platformBrowserDynamic'],
-			commonjs: '@angular/platform-browser-dynamic',
-			commonjs2: '@angular/platform-browser-dynamic',
-			amd: '@angular/platform-browser-dynamic'
-		},
-		'@angular/animations': {
-			root: ['ng', 'animations'],
-			commonjs: '@angular/animations',
-			commonjs2: '@angular/animations',
-			amd: '@angular/animations'
-		},
-		'@angular/platform-browser/animations': {
-			root: ['ng', 'platformBrowserAnimations'],
-			commonjs: '@angular/platform-browser/animations',
-			commonjs2: '@angular/platform-browser/animations',
-			amd: '@angular/platform-browser/animations'
-		},
-		'rxjs/Subject': {
-			root: ['rx', 'Subject'],
-			commonjs: 'rxjs/Subject',
-			commonjs2: 'rxjs/Subject',
-			amd: 'rxjs/Subject'
-		},
-		'rxjs/Observable': {
-			root: ['rx', 'Observable'],
-			commonjs: 'rxjs/Observable',
-			commonjs2: 'rxjs/Observable',
-			amd: 'rxjs/Observable'
-		},
-    'rxjs/BehaviorSubject': {
-      root: ['rx', 'BehaviorSubject'],
-      commonjs: 'rxjs/BehaviorSubject',
-      commonjs2: 'rxjs/BehaviorSubject',
-      amd: 'rxjs/BehaviorSubject'
-    }
-	},
+	externals: [
+        angularExternals(),
+        rxjsExternals()
+	],
 	module: {
 		rules: [
 			{
