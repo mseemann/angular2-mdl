83419546ffe067f3a163953b74ec7a6ccd2e302d
Michael Seemann <michael.seemann@synaos.com>
2020-12-20 09:54:38 +0100

    replace karma-coverage-instanbul-reporter with karma-coverage

--- projects/core/karma.conf.js
+++ projects/core/karma.conf.js
@@ -9,18 +9,20 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, '../../coverage/core'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+      reporters: [
+        {type: 'html'},
+        {type: 'lcovonly', subdir: '.', file: 'lcov.ino'},
+      ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
--- projects/datepicker/karma.conf.js
+++ projects/datepicker/karma.conf.js
@@ -9,18 +9,21 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, '../../coverage/datepicker'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+      reporters: [
+        // reporters not supporting the `file` property
+        {type: 'html'},
+        {type: 'lcovonly', subdir: '.', file: 'lcov.ino'},
+      ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
